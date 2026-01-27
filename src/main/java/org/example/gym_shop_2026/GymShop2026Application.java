eac8db56548bca39a1d6f74e94d0392de4323e77
OscarFigeac <figeacoscar63@gmail.com>
2026-01-27 20:23:26 +0000

    Initial commit

---
+++ src/main/java/org/example/gym_shop_2026/GymShop2026Application.java
@@ -0,0 +1,13 @@
+package org.example.gym_shop_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymShop2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymShop2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gym_shop_2026/GymShop2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gym_shop_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymShop2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
