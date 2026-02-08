85e6182a2109b2990b26f5ac4569109df1df050d
EoghanJPC <eoghanjpc@gmail.com>
2026-02-08 23:58:48 +0000

    Feat(DAO) - findByUsername Created in UserDAO

--- src/main/java/org/example/gym_shop_2026/persistence/UserDAO.java
+++ src/main/java/org/example/gym_shop_2026/persistence/UserDAO.java
@@ -1,9 +1,12 @@
 package org.example.gym_shop_2026.persistence;
 
+import org.example.gym_shop_2026.entities.Users;
+
 import java.sql.Date;
 import java.sql.SQLException;
 
 public interface UserDAO {
     boolean login(String username, String password) throws SQLException;
     boolean register(String username, String fullName, String userType, String eMail, String passWord, Date birthDay) throws SQLException;
+    Users findByUsername(String Username) throws SQLException;
 }
