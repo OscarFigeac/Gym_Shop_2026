61249bf9b6f33f5ce8488f2efbd7a2588bb6905c
EoghanJPC <eoghanjpc@gmail.com>
2026-02-08 06:21:33 +0000

    Feat(DAO) - PasswordHasher hashPassword and verifyPassword Creation

--- src/main/java/org/example/gym_shop_2026/utils/PasswordHasher.java
+++ src/main/java/org/example/gym_shop_2026/utils/PasswordHasher.java
@@ -1,4 +1,24 @@
 package org.example.gym_shop_2026.utils;
 
+
+
 public class PasswordHasher {
+    private static final int WORKLOAD = 12;
+
+    public static String hashPassword(String plainText){
+        if(plainText == null || plainText.isEmpty()){
+            throw new IllegalArgumentException("Unable to hash a password that has a NULL value or is empty !");
+        }
+
+        String salt = BCrypt.gensalt(WORKLOAD);
+        return BCrypt.hashpw(plainText, salt);
+    }
+
+    public static boolean verifyPassword(String plainText, String hashedPassword){
+        if(plainText == null || hashedPassword == null || hashedPassword.isEmpty()){
+            return false;
+        }
+
+        return BCrypt.checkpw(plainText, hashedPassword);
+    }
 }
