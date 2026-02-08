312f84b83c03a74a68cdae0c542659547bc18218
OscarFigeac <figeacoscar63@gmail.com>
2026-02-08 22:44:50 +0000

    Refactor (entities): PLEASE DO NOT USE the PLURAL TO REFER TO ENTITIES!!!

--- src/main/java/org/example/gym_shop_2026/entities/User.java
+++ src/main/java/org/example/gym_shop_2026/entities/User.java
@@ -3,16 +3,14 @@ package org.example.gym_shop_2026.entities;
 import lombok.*;
 
 
-import java.sql.Time;
 import java.util.Date;
-import java.util.Objects;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
 @Getter
 @Builder
 @AllArgsConstructor
-public class Users {
+public class User {
     @EqualsAndHashCode.Include
     @NonNull
     private int user_id;
