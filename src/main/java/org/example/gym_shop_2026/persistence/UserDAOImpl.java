51ea77be487250094cea555c2fb1a59db2c352fa
EoghanJPC <eoghanjpc@gmail.com>
2026-02-08 23:57:48 +0000

    Feat(DAO) - findByUsername Created in Impl

--- src/main/java/org/example/gym_shop_2026/persistence/UserDAOImpl.java
+++ src/main/java/org/example/gym_shop_2026/persistence/UserDAOImpl.java
@@ -2,7 +2,9 @@ package org.example.gym_shop_2026.persistence;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.gym_shop_2026.connector.Connector;
+import org.example.gym_shop_2026.entities.Users;
 import org.example.gym_shop_2026.utils.PasswordHasher;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 import org.example.gym_shop_2026.utils.PasswordHasher;
 
@@ -29,6 +31,7 @@ public class UserDAOImpl implements UserDAO{
                     String hashPassword = rs.getString("password");
                     return PasswordHasher.verifyPassword(pWord, hashPassword);
                 }
+                return false;
             }catch (SQLException e){
                 log.error("login() - An issue has arisen when the query was executed or processing the result set. Exception - {}", e.getMessage());
                 throw e;
@@ -83,7 +86,44 @@ public class UserDAOImpl implements UserDAO{
         return addedRows == 1;
     }
 
+    @Override
+    public Users findByUsername(String Username) throws SQLException{
+        if(stringValidation(Username)){
+            throw new IllegalArgumentException("Username - Field cannot be null or empty for search function.");
+        }
+
+        Connection conn = connector.getConnection();
+        if(conn == null){
+            throw new SQLException("findByUsername() - Unable to establish connection to the database !");
+        }
+
+        Users user = null;
+        try(PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE username = ?")){
+            ps.setString(1, Username);
+            try(ResultSet rs = ps.executeQuery()){
+                if(rs.next()) user = mapUserRow(rs);
+            }
+        } catch (SQLException e) {
+            log.error("findByUsername(): SQL error. \nException: {}", e.getMessage());
+            throw e;
+        }
+        return user;
+    }
+
+
     private static boolean stringValidation(String x){
         return x==null||x.isEmpty();
+    }
+
+    private static Users mapUserRow(ResultSet rs) throws SQLException {
+        return Users.builder()
+                .user_id(rs.getInt("user_id"))
+                .username(rs.getString("username"))
+                .fullName(rs.getString("fullName"))
+                .userType(rs.getString("userType"))
+                .email(rs.getString("email"))
+                .password(rs.getString("password"))
+                .dob(rs.getDate("dob"))
+                .build();
     }
 }
