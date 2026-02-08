9f1a76768bac833d45d2f5dd8be0ec99794e2c72
Cal Woods <D00270955@student.dkit.ie>
2026-02-08 16:30:45 +0000

    Refactor: Updated all references to Connector and removed in-line Connection object in method login() for UserDAOImpl class, as Connector implementation MySqlConnector holds the Connection.

--- src/main/java/org/example/gym_shop_2026/persistence/UserDAOImpl.java
+++ src/main/java/org/example/gym_shop_2026/persistence/UserDAOImpl.java
@@ -2,12 +2,15 @@ package org.example.gym_shop_2026.persistence;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.gym_shop_2026.connector.Connector;
+import org.example.gym_shop_2026.utils.PasswordHasher;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Repository
 @Slf4j
 public class UserDAOImpl implements UserDAO{
     private final Connector connector;
@@ -18,22 +21,21 @@ public class UserDAOImpl implements UserDAO{
 
     @Override
     public boolean login(String uName, String pWord) throws SQLException {
-        Connection conn = connector.getConnection();
-        if(conn == null){
+        if(connector == null){
             throw new SQLException("login() - Unable to establish a connection to the database !");
         }
-
-        try(PreparedStatement ps = conn.prepareStatement("SELECT * FROM users WHERE username = ?")){
+        try(PreparedStatement ps = connector.getConnection().prepareStatement("SELECT * FROM users WHERE username = ?")){
             ps.setString(1, uName);
             try(ResultSet rs = ps.executeQuery()){
                 if(rs.next()){
                     String hashPassword = rs.getString("password");
                     return PasswordHasher.verifyPassword(pWord, hashPassword);
                 }
-                return false;
             }catch (SQLException e){
                 log.error("login() - An issue has arisen when the query was executed or processing the result set. Exception - {}", e.getMessage());
             }
         }
+
+        return false;
     }
 }
