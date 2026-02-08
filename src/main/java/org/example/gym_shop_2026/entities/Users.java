0bd7c24e957f52c59c81d3080f7eb18dbcd2704a
EoghanJPC <eoghanjpc@gmail.com>
2026-02-08 05:57:38 +0000

    Feat(DTO) - User Entity Creation

---
+++ src/main/java/org/example/gym_shop_2026/entities/Users.java
@@ -0,0 +1,17 @@
+package org.example.gym_shop_2026.entities;
+
+import lombok.*;
+
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.Objects;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+public class Users {
+
+}
