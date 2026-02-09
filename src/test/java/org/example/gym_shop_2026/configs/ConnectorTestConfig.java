d07a504c14f7d77940e14677fea4dac13ce464c0
Cal Woods <D00270955@student.dkit.ie>
2026-02-09 10:40:35 +0000

    Feat: Added test configuration bean for Connector implementation in src/test/java/org.example.gym_shop_2026/configs folder.
    Added method connector() with @Bean annotation that returns a Connector implementation MySqlConnector with set attributes for: server, username, password and database.

--- src/test/java/org/example/gym_shop_2026/configs/ConnectorTestConfig.java
+++ src/test/java/org/example/gym_shop_2026/configs/ConnectorTestConfig.java
@@ -2,12 +2,10 @@ package org.example.gym_shop_2026.configs;
 
 import org.example.gym_shop_2026.connector.Connector;
 import org.example.gym_shop_2026.connector.MySqlConnector;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.bean.override.convention.TestBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@TestConfiguration
+@SpringBootConfiguration
 public class ConnectorTestConfig {
     @Bean
     public Connector connector() {
