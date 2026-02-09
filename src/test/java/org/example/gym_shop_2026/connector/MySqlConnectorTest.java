85d14fa1dea5ec521aa7a0deb45691a3d28a9d80
Cal Woods <D00270955@student.dkit.ie>
2026-02-09 10:40:08 +0000

    Refactor: Corrected MySqlConnector constructor that takes values for: server, username, password, database, as the connection string in DriverManager.getConnection() was not formatted correctly.
    Did not start with jdbc:mysql:// which was causing the MySqlConnector Connection instance object to come out null.
    
    Fixed as shown with successful Bean testing of MySqlConnector in MySqlConnectorTest class.

--- src/test/java/org/example/gym_shop_2026/connector/MySqlConnectorTest.java
+++ src/test/java/org/example/gym_shop_2026/connector/MySqlConnectorTest.java
@@ -1,33 +1,34 @@
 package org.example.gym_shop_2026.connector;
 
+import org.example.gym_shop_2026.GymShop2026Application;
+import org.example.gym_shop_2026.GymShop2026ApplicationTests;
 import org.example.gym_shop_2026.configs.ConnectorTestConfig;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MySqlConnectorTest {
-    @Mock
+
+@SpringBootTest(classes = ConnectorTestConfig.class)
+public class MySqlConnectorTest {
+    @Autowired
     private Connector connector;
 
     @Test
-    void freeConnectionReturnsTrue() throws SQLException {
-        assertTrue(this.connector.freeConnection() == true);
+    void getConnectionReturnsValidConnection() throws SQLException {
+        assertNotNull(connector);
+        assertNotNull(connector.getConnection());
+        assertTrue(connector.getConnection().isValid(6));
     }
 
     @Test
-    void getConnectionReturnsValidConnection() throws SQLException {
-        Connection connection = this.connector.getConnection();
-
-        assertNotNull(connection);
-        assertTrue(connection.isValid(6));
+    void freeConnectionReturnsTrueWithValidConnector() {
+        assertNotNull(connector);
+        assertNotNull(connector.getConnection());
+        assertTrue(connector.freeConnection());
     }
 }
