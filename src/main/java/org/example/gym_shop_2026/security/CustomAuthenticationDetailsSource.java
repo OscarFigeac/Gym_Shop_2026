3322f25110fb26dbb46b170661767e668edeb25a
OscarFigeac <figeacoscar63@gmail.com>
2026-02-09 00:21:46 +0000

    feat (Security): added component tag to CustomAuthenticationDetailsSource file for autowiring

--- src/main/java/org/example/gym_shop_2026/security/CustomAuthenticationDetailsSource.java
+++ src/main/java/org/example/gym_shop_2026/security/CustomAuthenticationDetailsSource.java
@@ -3,7 +3,9 @@ package org.example.gym_shop_2026.security;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomAuthenticationDetailsSource
         implements AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
 
