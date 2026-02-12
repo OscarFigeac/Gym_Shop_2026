02fb2c44a8ec2950e203e4110c13b304c2a2eb9f
EoghanJPC <eoghanjpc@gmail.com>
2026-02-12 10:13:37 +0000

    Feat(Service) - UserService Set Up

--- src/main/java/org/example/gym_shop_2026/services/UserService.java
+++ src/main/java/org/example/gym_shop_2026/services/UserService.java
@@ -1,4 +1,15 @@
 package org.example.gym_shop_2026.services;
 
+import org.example.gym_shop_2026.persistence.UserDAO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserDAO userDao;
+
+    public UserService(UserDAO userDao){
+        this.userDao = userDao;
+    }
+
+
 }
