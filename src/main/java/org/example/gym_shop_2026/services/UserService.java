b0e255ea120abcb6b99c5c31bb20a36380e17f34
EoghanJPC <eoghanjpc@gmail.com>
2026-02-12 10:16:06 +0000

    Feat(Service) - UserService Login Method

--- src/main/java/org/example/gym_shop_2026/services/UserService.java
+++ src/main/java/org/example/gym_shop_2026/services/UserService.java
@@ -3,12 +3,18 @@ package org.example.gym_shop_2026.services;
 import org.example.gym_shop_2026.persistence.UserDAO;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+
 @Service
 public class UserService {
     private final UserDAO userDao;
 
     public UserService(UserDAO userDao){
         this.userDao = userDao;
+    }
+
+    public boolean loginUser(String username, String password) throws SQLException {
+        return userDao.login(username, password);
     }
 
 
