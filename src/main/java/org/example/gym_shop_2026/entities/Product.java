e74aedc0a18285a764c3e081881df607dd200d35
OscarFigeac <figeacoscar63@gmail.com>
2026-02-24 21:07:26 +0000

    feat(entities): product entity added with validation

---
+++ src/main/java/org/example/gym_shop_2026/entities/Product.java
@@ -0,0 +1,34 @@
+package org.example.gym_shop_2026.entities;
+
+import lombok.*;
+import jakarta.validation.constraints.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @EqualsAndHashCode.Include
+    private final int productId;
+
+    @NotBlank(message = "Category is required")
+    @NonNull
+    private final String productCategory;
+
+    @NotBlank(message = "Product name is required")
+    @NonNull
+    private final String name;
+
+    @NotBlank(message = "Description is required")
+    @NonNull
+    private final String description;
+
+    @Min(value = 0, message = "In stock quantity cannot be negative")
+    @NonNull
+    private final int inStock;
+
+    @NonNull
+    private final int storeId;
+}
