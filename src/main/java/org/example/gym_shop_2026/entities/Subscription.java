e6c83bb7195cc0707788908236940f83e7a049b7
Cal Woods <D00270955@student.dkit.ie>
2026-03-04 14:14:39 +0000

    Feat: Added entity subscriptions to serve as a dto for subscriptions database table.
    Includes boolean equals(Object obj) and int hashcode() override methods, String toString() override, getters/setters except no setter on planId and two constructors: one that takes argument parameters for all attributes and another that takes arguments for attributes marked with org.projectlombok.lombok.NonNull annotation.

---
+++ src/main/java/org/example/gym_shop_2026/entities/Subscription.java
@@ -0,0 +1,24 @@
+package org.example.gym_shop_2026.entities;
+
+import lombok.*;
+
+/**
+ * An entity representing subscription records in subscriptions persistence.
+ *
+ * @author Cal Woods
+ */
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+    @Setter(AccessLevel.NONE)
+    @EqualsAndHashCode.Include private int planId;
+
+    @NonNull private String description;
+    @NonNull private String planName;
+    @NonNull private double planPrice;
+    @NonNull private int planDuration;
+}
