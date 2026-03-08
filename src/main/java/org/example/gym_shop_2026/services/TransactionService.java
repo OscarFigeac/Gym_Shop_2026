f6bddad89743e644763bc5053b2b3f1a214ee153
Cal Woods <D00270955@student.dkit.ie>
2026-03-08 20:34:03 +0000

    Refactor: Commented method public List<Transaction> getAllTransactionsForUser(int user_id) pending further storage review.

--- src/main/java/org/example/gym_shop_2026/services/TransactionService.java
+++ src/main/java/org/example/gym_shop_2026/services/TransactionService.java
@@ -14,7 +14,29 @@ import org.springframework.stereotype.Service;
 public class TransactionService {
     private TransactionDAO transactionDAO;
 
+    /**
+     * Constructor for use by Spring when auto-wiring service class with given {@link TransactionDAO}.
+     * @param transactionDAO Given {@link TransactionDAO} implementation
+     */
     public TransactionService(TransactionDAO transactionDAO) {
         this.transactionDAO = transactionDAO;
     }
-}
+//
+//    /**
+//     * Provides all transactions for a user with matching id
+//     * @param user_id Given id of user in system
+//     * @return {@link List<Transaction>} objects with data from configured {@link TransactionDAO}
+//     */
+//    public List<Transaction> getAllTransactionsForUser(int user_id) {
+//        if(user_id <= 0) {
+//            log.error("Cannot get transactions for user with ID " + user_id + " as given user id must be > 0!");
+//            throw new IllegalArgumentException("Cannot get transactions for user with ID " + user_id + " as given user id must be > 0!");
+//
+//            try {
+//                transactionDAO.getTransactions();
+//            }
+//            catch (SQLException e) {
+//
+//            }
+//        }
+    }
