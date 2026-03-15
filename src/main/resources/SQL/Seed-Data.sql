31590b6ddbaf65d791180e63295ecefb53e14973
Cal Woods <D00270955@student.dkit.ie>
2026-03-15 11:26:33 +0000

    Refactor(SQL): Removed parenthesis at-top of INSERT statement as it was incorrect SQL syntax.
    Statement is now running properly.

--- src/main/resources/SQL/Seed-Data.sql
+++ src/main/resources/SQL/Seed-Data.sql
@@ -1,8 +1,7 @@
-INSERT INTO products(product_category, name, price, quantity) VALUES(
-    ("Supplements", "Vanilla Protein", 25.99, 12),
-    ("Equipment", "Weights(x2) 8KGS", 41.99, 35),
-    ("Equipment", "Threadmill", 89.99, 8),
-    ("Supplements", "Two-week weight loss pack", 34.99, 15),
-    ("Supplements", "Origin Pump Pre-Workout | Stim & Caffeine-Free", 23.99, 24),
-    ("Equipment", "Hex Dumbbells(x2) 55KGS", 324.99, 4)
-);
+INSERT INTO products (product_category, name, price, quantity) VALUES(
+    'Supplements', 'Vanilla Protein', 25.99, 12),
+    ('Equipment', 'Weights(x2) 8KGS', 41.99, 35),
+    ('Equipment', 'Threadmill', 89.99, 8),
+    ('Supplements', 'Two-week weight loss pack', 34.99, 15),
+    ('Supplements', 'Origin Pump Pre-Workout | Stim & Caffeine-Free', 23.99, 24),
+    ('Equipment', 'Hex Dumbbells(x2) 55KGS', 324.99, 4);
