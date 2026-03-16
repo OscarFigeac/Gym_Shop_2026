ddf149bf8249d598af858806b82b2205664a56f1
OscarFigeac <figeacoscar63@gmail.com>
2026-03-16 19:16:03 +0000

    feat(persistence): Incomplete Basket Logic completed.

--- src/main/java/org/example/gym_shop_2026/persistence/BasketDAO.java
+++ src/main/java/org/example/gym_shop_2026/persistence/BasketDAO.java
@@ -1,9 +1,15 @@
 package org.example.gym_shop_2026.persistence;
 
 import org.example.gym_shop_2026.entities.Basket;
+import org.example.gym_shop_2026.entities.BasketItem;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface BasketDAO {
     Basket findByUserID(int user_id) throws SQLException;
+    public List<BasketItem> findItemsByBasketId(int basketId) throws SQLException;
+    void addItemToBasket(int basketId, int productId, int quantity) throws SQLException;
+    void updateItemQuantity(int itemId, int newQuantity) throws SQLException;
+    void removeItem(int itemId) throws SQLException;
 }
--- src/main/java/org/example/gym_shop_2026/persistence/BasketDAOImpl.java
+++ src/main/java/org/example/gym_shop_2026/persistence/BasketDAOImpl.java
@@ -3,6 +3,7 @@ package org.example.gym_shop_2026.persistence;
 import lombok.extern.slf4j.Slf4j;
 import org.example.gym_shop_2026.connector.Connector;
 import org.example.gym_shop_2026.entities.Basket;
+import org.example.gym_shop_2026.entities.BasketItem;
 import org.example.gym_shop_2026.entities.User;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @Slf4j
@@ -46,6 +49,90 @@ public class BasketDAOImpl implements BasketDAO {
         return found;
     }
 
+    /**
+     * @author Oscar
+     * Takes in a basket Id to display the content of the basket.
+     * @param basketId The basket being looked for
+     * @return A List of Items stored in the Basket
+     * @throws SQLException If the Connection to the database fails at any point
+     */
+    @Override
+    public List<BasketItem> findItemsByBasketId(int basketId) throws SQLException {
+        List<BasketItem> items = new ArrayList<>();
+        String sql = "SELECT * FROM basket_item WHERE basketId = ?";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, basketId);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    items.add(BasketItem.builder()
+                            .itemId(rs.getInt("itemId"))
+                            .productId(rs.getInt("productId"))
+                            .basketId(rs.getInt("basketId"))
+                            .itemQuantity(rs.getInt("itemQuantity"))
+                            .build());
+                }
+            }
+        }
+        return items;
+    }
+
+    /**
+     * @author Oscar
+     * Adds an item into the basket, it's id and quantity
+     * @param basketId The basket identifier where the products are being added
+     * @param productId The Identifier of the product being added
+     * @param quantity The number of Items being added
+     * @throws SQLException If the connection to the database fails at any point
+     */
+    @Override
+    public void addItemToBasket(int basketId, int productId, int quantity) throws SQLException {
+        String sql = "INSERT INTO basket_item (basket_id, product_id, item_quantity) " +
+                "VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE item_quantity = item_quantity + ?";
+
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, basketId);
+            ps.setInt(2, productId);
+            ps.setInt(3, quantity);
+            ps.setInt(4, quantity);
+            ps.executeUpdate();
+        }
+    }
+
+    /**
+     * @author Oscar
+     * Updates the amount of an item currently in a basket
+     * @param itemId The item being updated
+     * @param newQuantity The new amount being added into the basket
+     * @throws SQLException If the connection to the database fails at any point
+     */
+    @Override
+    public void updateItemQuantity(int itemId, int newQuantity) throws SQLException {
+        String sql = "UPDATE basket_item SET item_quantity = ? WHERE item_id = ?";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, newQuantity);
+            ps.setInt(2, itemId);
+            ps.executeUpdate();
+        }
+    }
+
+    /**
+     * @author Oscar
+     * Deletes an item from a basket
+     * @param itemId The item being deleted
+     * @throws SQLException If the connection to the database fails at any point
+     */
+    @Override
+    public void removeItem(int itemId) throws SQLException {
+        String sql = "DELETE FROM basket_item WHERE item_id = ?";
+        try (Connection conn = connector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, itemId);
+            ps.executeUpdate();
+        }
+    }
     //Private Methods
 
     private static Basket mapBasketRow(ResultSet rs) throws SQLException {
