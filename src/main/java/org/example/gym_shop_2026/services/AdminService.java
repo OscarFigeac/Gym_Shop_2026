3644f938fd8fb80051b8272fa7719b0f81900e1c
EoghanJPC <eoghanjpc@gmail.com>
2026-03-20 21:02:43 +0000

    Feat(Services) - AdminService Function Population

--- src/main/java/org/example/gym_shop_2026/services/AdminService.java
+++ src/main/java/org/example/gym_shop_2026/services/AdminService.java
@@ -1,8 +1,12 @@
 package org.example.gym_shop_2026.services;
 
 
+import org.example.gym_shop_2026.entities.Product;
 import org.example.gym_shop_2026.persistence.ProductDAO;
 import org.example.gym_shop_2026.persistence.ProductDAOImpl;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class AdminService {
 
@@ -12,5 +16,11 @@ public class AdminService {
         this.pDAO = pD;
     }
 
+    public List<Product> getReorderProducts() throws SQLException{
+        return pDAO.getProductsLowStock(2);
+    }
 
+    public List<Product> getBestSellers() throws SQLException{
+        return pDAO.getBestSellers(10);
+    }
 }
