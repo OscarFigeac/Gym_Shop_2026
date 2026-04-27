6f9b96c2910fe088d3af888e357dcf2ff77b4833
OscarFigeac <figeacoscar63@gmail.com>
2026-04-27 23:13:54 +0100

    feat(controllers): added controller and mapping annotations to the top of the file

--- src/main/java/org/example/gym_shop_2026/controllers/DashboardController.java
+++ src/main/java/org/example/gym_shop_2026/controllers/DashboardController.java
@@ -4,13 +4,17 @@ import org.example.gym_shop_2026.entities.User;
 import org.example.gym_shop_2026.services.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.sql.SQLException;
 
+@Controller
+@RequestMapping("/dashboard")
 public class DashboardController {
 
     private final UserService userService;
