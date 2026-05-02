05bd6758423398ff74425b79ff8022c42193fbbe
EoghanJPC <eoghanjpc@gmail.com>
2026-05-02 02:29:17 +0100

    Feat(Service) - getAllTransactions brought to the admin to be used

--- src/main/java/org/example/gym_shop_2026/services/AdminService.java
+++ src/main/java/org/example/gym_shop_2026/services/AdminService.java
@@ -2,11 +2,12 @@ package org.example.gym_shop_2026.services;
 
 
 import org.example.gym_shop_2026.entities.Product;
+import org.example.gym_shop_2026.entities.Transaction;
 import org.example.gym_shop_2026.persistence.ProductDAO;
-import org.example.gym_shop_2026.persistence.ProductDAOImpl;
+import org.example.gym_shop_2026.persistence.TransactionDAO;
 import org.springframework.stereotype.Service;
-
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -14,9 +15,11 @@ import java.util.List;
 public class AdminService {
 
     public final ProductDAO pDAO;
+    public final TransactionDAO tDAO;
 
-    public AdminService(ProductDAO pD){
+    public AdminService(ProductDAO pD, TransactionDAO tD){
         this.pDAO = pD;
+        this.tDAO = tD;
     }
 
     public List<Product> getReorderProducts() throws SQLException{
@@ -25,5 +28,13 @@ public class AdminService {
 
     public List<Product> getBestSellers() throws SQLException{
         return pDAO.getBestSellers(10);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        try {
+            return tDAO.getAllTransactions();
+        } catch (SQLException e) {
+            return Collections.emptyList();
+        }
     }
 }
