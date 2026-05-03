7d272c70623383eb67f5502c265860c55c41f411
Cal Woods <D00270955@student.dkit.ie>
2026-05-03 20:16:33 +0100

    Docs: Changed javadocs for all-args constructor.
    Now updated with new parent class constructor info.

--- src/main/java/org/example/gym_shop_2026/entities/OAuthClientRegistrars.java
+++ src/main/java/org/example/gym_shop_2026/entities/OAuthClientRegistrars.java
@@ -24,6 +24,13 @@ public abstract class OAuthClientRegistrars {
     /**
      * An all-arguments constructor that sets field attributes to parameterised
      * values.
+     * @param registrationId Url of authorisation server
+     * @param clientId Client id for authorisation server
+     * @param clientSecret Client secret from provider client setup
+     * @param oAuthMethod Method of authorisation, an enum {@link OAuthMethod}
+     * @param redirectUri Resource for further redirection
+     * @param scopes Permission scopes
+     * @param grantTypes Types of granted privileges issued
      */
     protected OAuthClientRegistrars(String registrationId, String clientId, String clientSecret, OAuthMethod oAuthMethod, String redirectUri, String scopes, String grantTypes) {
         this.registrationId = registrationId;
